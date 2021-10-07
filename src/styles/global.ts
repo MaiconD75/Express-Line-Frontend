ea68ffe3fed3d202b827feef887d9364d38d4b3c
maicond75 <maicondouglas75@hotmail.com>
2021-10-07 11:43:22 -0300

    add/scrollbar-style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,6 +9,7 @@ export default createGlobalStyle`
 
   body {
     background: #f5f5f5;
+    overflow: hidden;
   }
 
   body,
@@ -24,5 +25,22 @@ export default createGlobalStyle`
   em {
     font: 20px 'Roboto', sans-serif;
     color: #ccc;
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+    z-index: 10;
+  }
+
+  ::-webkit-scrollbar-track{
+    background: #fefefe;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #cecece;
+    border-radius: 4px;
   }
 `;
